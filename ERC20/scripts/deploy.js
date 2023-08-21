8586560bafcd964073938f3b344abba4a5c02384
ledanghuy1811 <huygamo567@gmail.com>
2023-08-21 15:09:28 +0700

    update deploy script for ERC20

--- ERC20/scripts/deploy.js
+++ ERC20/scripts/deploy.js
@@ -2,21 +2,27 @@
 const hre = require("hardhat");
 
 async function main() {
-    // Using `hre` - Hardhat Runtime Environment - we find and deploy
-  	// a contract named `Crypto`
-    const cryptoContract = await hre.ethers.deployContract("Crypto");
+	// Using `hre` - Hardhat Runtime Environment - we find and deploy
+	// a contract named `Crypto`
+	const cryptoContract = await hre.ethers.deployContract("Crypto");
 
-    // We wait for the contract to finish deploying
-    await cryptoContract.waitForDeployment();
+	// We wait for the contract to finish deploying
+	// await cryptoContract.waitForDeployment();
 
-    // We print the address of the deployed contract to our console
-    console.log("Crypto Contract Address:", cryptoContract.target);
+	// We print the address of the deployed contract to our console
+	console.log("Crypto Contract Address:", cryptoContract.target);
+
+	const stakingContract = await hre.ethers.deployContract("Staking", [
+		cryptoContract.target,
+	]);
+	// await stakingContract.waitForDeployment();
+	console.log("Staking Contract Address:", stakingContract.target);
 }
 
 // Call the main function and catch if there is any error
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
