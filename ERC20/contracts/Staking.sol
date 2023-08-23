4796e5dd15faca8054881ba59bb57ac393560a7d
ledanghuy1811 <huygamo567@gmail.com>
2023-08-24 00:30:20 +0700

    update staking creator contract and test case

--- ERC20/contracts/Staking.sol
+++ ERC20/contracts/Staking.sol
@@ -5,6 +5,23 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract StakingCreator {
+    Staking[] private stakes;
+
+    function createStaking(address tokenAddress) public {
+        Staking newStake = new Staking(tokenAddress, msg.sender);
+        stakes.push(newStake);
+    }
+
+    function getStakeByIndex(uint256 index) public view returns (Staking) {
+        return Staking(stakes[index]);
+    }
+
+    function getOwner(uint256 index) public view returns (address) {
+        return stakes[index].owner();
+    }
+}
+
 contract Staking is Ownable {
     uint256 private constant secondPerDay = 86400; // second in a day
     IERC20 private token;
@@ -16,11 +33,12 @@ contract Staking is Ownable {
     }
     mapping(address => StakeInfo) private userStakeInfo; // stake amount of each token holder
     uint256 private totalStakeAmount; // total stake amount of all token holder
-    bool private isInitialize;
-    uint256 private stakingContractToken;
+    bool private isInitialize; // initialize once
+    uint256 private stakingContractToken; // pool stake token
 
-    constructor(address tokenAddr) Ownable() {
+    constructor(address tokenAddr, address stakeOwner) Ownable() {
         token = IERC20(tokenAddr); // address of token contract
+        transferOwnership(stakeOwner); // address of stake owner
     }
 
     function initialize() public onlyOwner {
@@ -51,6 +69,11 @@ contract Staking is Ownable {
             require(
                 userStakeInfo[msg.sender].dayEnd > block.timestamp,
                 "Staking: can not stake after 30 days!"
+            );
+        } else {
+            require(
+                token.balanceOf(address(this)) > 0,
+                "Staking: nothing to claim after stake!"
             );
         }
 
--- ERC20/test/Crypto.js
+++ ERC20/test/Crypto.js
@@ -6,25 +6,20 @@ describe("Crypto and Staking:", () => {
 	let owner;
 	let addr1;
 	let addr2;
+	let addr3;
 	let addrs;
 	let cryptoContract;
-	let stakingContract;
-	let stakingToken;
+	let stakingCreatorContract;
+	let ownerStakingContract;
+	let addr1StakingContract;
+	let ownerStakingToken;
+	let addr1StakingToken;
 
 	before(async () => {
-		[owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+		[owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
 
 		cryptoContract = await ethers.deployContract("Crypto");
-		stakingContract = await ethers.deployContract("Staking", [
-			cryptoContract.target,
-		]);
-
-		stakingToken =
-			((await cryptoContract.balanceOf(owner.address)) * BigInt(3)) /
-			BigInt(10);
-
-		await cryptoContract.setWhiteList(stakingContract.target);
-		await cryptoContract.approve(stakingContract.target, stakingToken);
+		stakingCreatorContract = await ethers.deployContract("StakingCreator");
 	});
 
 	describe("Crypto:", () => {
@@ -55,66 +50,117 @@ describe("Crypto and Staking:", () => {
 			before(async () => {
 				await cryptoContract.setWhiteList(addr1.address);
 				await cryptoContract.setWhiteList(addr2.address);
+				await cryptoContract.setWhiteList(addr3.address);
 			});
 
 			it("Should receive more than 1 million token", async () => {
-				const tokenHolderAddress = addr1.address;
 				const amount = 1000000;
 
-				await cryptoContract.transfer(tokenHolderAddress, amount);
+				await cryptoContract.transfer(addr1.address, amount);
 				await cryptoContract.transfer(addr2.address, amount);
+				await cryptoContract.transfer(addr3.address, amount);
 			});
 		});
 	});
 
 	describe("Staking:", () => {
+		before(async () => {
+			await stakingCreatorContract.createStaking(cryptoContract.target); // 1 pool by owner
+			let addr = await stakingCreatorContract.getStakeByIndex(0);
+			ownerStakingContract = await ethers.getContractAt("Staking", addr);
+			ownerStakingToken =
+				((await cryptoContract.balanceOf(owner.address)) * BigInt(3)) /
+				BigInt(10);
+			await cryptoContract.setWhiteList(ownerStakingContract.target);
+			await cryptoContract.approve(
+				ownerStakingContract.target,
+				ownerStakingToken
+			);
+
+			let tx = await stakingCreatorContract.connect(addr1);
+			await tx.createStaking(cryptoContract.target); // 1 pool by addr1
+			addr = await tx.getStakeByIndex(1);
+			addr1StakingContract = await ethers.getContractAt("Staking", addr);
+			tx = await cryptoContract.connect(addr1);
+			addr1StakingToken =
+				((await tx.balanceOf(addr1.address)) * BigInt(3)) / BigInt(10);
+			await tx.approve(addr1StakingContract.target, addr1StakingToken);
+		});
+
 		describe("Initialize staking token:", () => {
 			it("Should initialize by owner", async () => {
-				const tx = await stakingContract.connect(addr1);
+				const tx1 = await ownerStakingContract.connect(addr2);
+				const tx2 = await addr1StakingContract.connect(addr2);
 
-				await expect(tx.initialize()).to.be.revertedWith(
+				await expect(tx1.initialize()).to.be.revertedWith(
+					"Ownable: caller is not the owner"
+				);
+				await expect(tx2.initialize()).to.be.revertedWith(
 					"Ownable: caller is not the owner"
 				);
 			});
 
 			it("Should initialize once time", async () => {
-				await stakingContract.initialize();
+				const tx = addr1StakingContract.connect(addr1);
+				await ownerStakingContract.initialize();
+				await tx.initialize();
 
-				await expect(stakingContract.initialize()).to.be.revertedWith(
+				await expect(ownerStakingContract.initialize()).to.be.revertedWith(
+					"Staking: initialized!"
+				);
+				await expect(tx.initialize()).to.be.revertedWith(
 					"Staking: initialized!"
 				);
 			});
 
 			it("Should initialize with 30% of token", async () => {
-				const stakingBalance = await cryptoContract.balanceOf(
-					stakingContract.target
+				const ownerStakingBalance = await cryptoContract.balanceOf(
+					ownerStakingContract.target
+				);
+				const addr1StakingBalance = await cryptoContract.balanceOf(
+					addr1StakingContract.target
 				);
 
-				expect(stakingBalance).to.equal(stakingToken);
+				expect(ownerStakingBalance).to.equal(ownerStakingToken);
+				expect(addr1StakingBalance).to.equal(addr1StakingToken);
 			});
 		});
 
 		describe("Staking token:", () => {
 			before(async () => {
-				const addr1Balance = await cryptoContract.balanceOf(addr1.address);
-				const tx = await cryptoContract.connect(addr1);
+				const addr2Balance = await cryptoContract.balanceOf(addr2.address);
+				let tx = await cryptoContract.connect(addr2);
+				await tx.approve(ownerStakingContract.target, addr2Balance);
+				await tx.approve(addr1StakingContract.target, addr2Balance);
 
-				await tx.approve(stakingContract.target, addr1Balance);
+				const addr3Balance = await cryptoContract.balanceOf(addr3.address);
+				tx = await cryptoContract.connect(addr3);
+				await tx.approve(ownerStakingContract.target, addr3Balance);
+				await tx.approve(addr1StakingContract.target, addr3Balance);
 			});
 
 			it("Should not stake token more than current balance!", async () => {
-				const tx = await stakingContract.connect(addr1);
 				const amount = 2000000;
 
+				let tx = await ownerStakingContract.connect(addr2);
+				await expect(tx.stakeToken(amount)).to.be.revertedWith(
+					"ERC20: insufficient allowance"
+				);
+
+				tx = await addr1StakingContract.connect(addr2);
 				await expect(tx.stakeToken(amount)).to.be.revertedWith(
 					"ERC20: insufficient allowance"
 				);
 			});
 
 			it("Should stake more than one time!", async () => {
-				const tx = await stakingContract.connect(addr1);
 				const amount = 1000;
 
+				let tx = await ownerStakingContract.connect(addr2);
+				await tx.stakeToken(amount);
+				await tx.stakeToken(amount);
+
+				tx = await addr1StakingContract.connect(addr2);
 				await tx.stakeToken(amount);
 				await tx.stakeToken(amount);
 			});
@@ -122,17 +168,20 @@ describe("Crypto and Staking:", () => {
 
 		describe("Claim token:", () => {
 			before(async () => {
-				const addr2Balance = await cryptoContract.balanceOf(addr2.address);
-				let tx = await cryptoContract.connect(addr2);
-				await tx.approve(stakingContract.target, addr2Balance);
+				let tx = await ownerStakingContract.connect(addr3);
+				await tx.stakeToken(3000);
 
-				tx = await stakingContract.connect(addr2);
+				tx = await addr1StakingContract.connect(addr3);
 				await tx.stakeToken(3000);
 			});
 
 			it("Should not claim token before 30 days!", async () => {
-				const tx = await stakingContract.connect(addr1);
+				let tx = await ownerStakingContract.connect(addr2);
+				await expect(tx.claimToken()).to.be.revertedWith(
+					"Staking: not enough 30 days!"
+				);
 
+				tx = await addr1StakingContract.connect(addr2);
 				await expect(tx.claimToken()).to.be.revertedWith(
 					"Staking: not enough 30 days!"
 				);
@@ -141,150 +190,71 @@ describe("Crypto and Staking:", () => {
 			it("Should claim token after 30 days!", async () => {
 				const claimTime = (await time.latest()) + 86400 * 31; // time after 30 days
 				await time.increaseTo(claimTime);
-				const tx = await stakingContract.connect(addr1);
 
+				let tx = await ownerStakingContract.connect(addr2);
+				await tx.claimToken();
+
+				tx = await addr1StakingContract.connect(addr2);
 				await tx.claimToken();
 			});
 
 			it("Should take bonus after claim token!", async () => {
-				const addrBalance = await cryptoContract.balanceOf(addr2.address);
-				const tokenStaked = await stakingContract.getStakedAmount(
+				let addrBalance = await cryptoContract.balanceOf(addr2.address);
+				let tokenStaked = await ownerStakingContract.getStakedAmount(
 					addr2.address
 				);
-				const totalStakeAmount = await stakingContract.getTotalStakedToken();
-				const stakingContractToken =
-					await stakingContract.getStakingContractToken();
-
-				const claimToken =
+				let totalStakeAmount = await ownerStakingContract.getTotalStakedToken();
+				let stakingContractToken =
+					await ownerStakingContract.getStakingContractToken();
+				let claimToken =
 					addrBalance +
 					tokenStaked +
 					(tokenStaked * stakingContractToken) / totalStakeAmount;
-				const tx = await stakingContract.connect(addr2);
-				await tx.claimToken();
 
+				let tx = await ownerStakingContract.connect(addr2);
+				await tx.claimToken();
 				expect(await cryptoContract.balanceOf(addr2.address)).to.equal(
+					claimToken
+				);
+
+				addrBalance = await cryptoContract.balanceOf(addr3.address);
+				tokenStaked = await addr1StakingContract.getStakedAmount(addr3.address);
+				totalStakeAmount = await addr1StakingContract.getTotalStakedToken();
+				stakingContractToken =
+					await addr1StakingContract.getStakingContractToken();
+				claimToken =
+					addrBalance +
+					tokenStaked +
+					(tokenStaked * stakingContractToken) / totalStakeAmount;
+
+				tx = await addr1StakingContract.connect(addr3);
+				await tx.claimToken();
+				expect(await cryptoContract.balanceOf(addr3.address)).to.equal(
 					claimToken
 				);
 			});
 
 			it("Should not stake token after 30 days!", async () => {
-				const tx = await stakingContract.connect(addr1);
 				const amount = 1000;
 
+				let tx = await ownerStakingContract.connect(addr2);
+				await expect(tx.stakeToken(amount)).to.be.revertedWith(
+					"Staking: can not stake after 30 days!"
+				);
+
+				tx = await addr1StakingContract.connect(addr2);
 				await expect(tx.stakeToken(amount)).to.be.revertedWith(
 					"Staking: can not stake after 30 days!"
 				);
 			});
+
+			it("Should not stake token when pool is empty!", async () => {
+				const amount = 1000;
+
+				await expect(
+					addr1StakingContract.stakeToken(amount)
+				).to.be.revertedWith("Staking: nothing to claim after stake!");
+			});
 		});
 	});
-
-	// describe("lock()", () => {
-	//   before(async () => {
-	//     let tx = await cryptoContract
-	//       .connect(addr1)
-	//       .approve(
-	//         stakingContract.address,
-	//         ethers.utils.parseEther("1000").toString()
-	//       );
-	//     await tx.wait();
-
-	//     tx = await stakingContract.connect(addr1).lock({
-	//       token: cryptoContract.address,
-	//       amount: ethers.utils.parseEther("1000").toString(),
-	//       startTime: Math.ceil(new Date().getTime() / 1000),
-	//       duration: 86400, // one day
-	//       owner: addr1.address,
-	//     });
-	//     await tx.wait();
-	//   });
-
-	//   it("Check lock data", async () => {
-	//     const nonce = await stakingContract.nonce(addr1.address);
-	//     const idx = await stakingContract.getLockIdx(
-	//       addr1.address,
-	//       parseInt(nonce) - 1
-	//     );
-	//     const data = await stakingContract.lockData(idx);
-	//     expect(data.token).to.be.eq(cryptoContract.address);
-	//     expect(data.owner).to.be.eq(addr1.address);
-	//     expect(data.amount.toString()).to.be.eq(
-	//       ethers.utils.parseEther("1000").toString()
-	//     );
-	//   });
-
-	//   it("Check can claimable", async () => {
-	//     const nonce = await stakingContract.nonce(addr1.address);
-	//     const idx = await stakingContract.getLockIdx(
-	//       addr1.address,
-	//       parseInt(nonce) - 1
-	//     );
-	//     const data = await stakingContract.isClaimable(idx);
-	//     expect(data).to.be.eq(false);
-	//   });
-	// });
-
-	// describe("unlock()", () => {
-	//   before(async () => {
-	//     let tx = await cryptoContract
-	//       .connect(addr2)
-	//       .approve(
-	//         stakingContract.address,
-	//         ethers.utils.parseEther("1000").toString()
-	//       );
-	//     await tx.wait();
-
-	//     const blockNumBefore = await ethers.provider.getBlockNumber();
-	//     const blockBefore = await ethers.provider.getBlock(blockNumBefore);
-	//     const timestamp = blockBefore.timestamp;
-
-	//     tx = await stakingContract.connect(addr2).lock({
-	//       token: cryptoContract.address,
-	//       amount: ethers.utils.parseEther("1000").toString(),
-	//       startTime: timestamp,
-	//       duration: 86400, // one day
-	//       owner: addr2.address,
-	//     });
-	//     await tx.wait();
-
-	//     await time.increaseTo(timestamp + 86400);
-
-	//     const nonce = await stakingContract.nonce(addr2.address);
-	//     const idx = await stakingContract.getLockIdx(
-	//       addr2.address,
-	//       parseInt(nonce) - 1
-	//     );
-	//     tx = await stakingContract.connect(addr2).unlock(idx);
-	//   });
-
-	//   it("Check lock data", async () => {
-	//     const nonce = await stakingContract.nonce(addr2.address);
-	//     const idx = await stakingContract.getLockIdx(
-	//       addr2.address,
-	//       parseInt(nonce) - 1
-	//     );
-	//     const data = await stakingContract.lockData(idx);
-	//     expect(data.token).to.be.eq(cryptoContract.address);
-	//     expect(data.owner).to.be.eq(addr2.address);
-	//     expect(data.amount.toString()).to.be.eq(
-	//       ethers.utils.parseEther("1000").toString()
-	//     );
-	//   });
-
-	//   it("Check can claimable", async () => {
-	//     const nonce = await stakingContract.nonce(addr2.address);
-	//     const idx = await stakingContract.getLockIdx(
-	//       addr2.address,
-	//       parseInt(nonce) - 1
-	//     );
-	//     const data = await stakingContract.isClaimable(idx);
-	//     expect(data).to.be.eq(true);
-	//   });
-
-	//   it("Check balance of unlocker", async () => {
-	//     const balance = await cryptoContract.balanceOf(addr2.address);
-	//     expect(balance.toString()).to.be.eq(
-	//       ethers.utils.parseEther("1000").toString()
-	//     );
-	//   });
-	// });
 });
